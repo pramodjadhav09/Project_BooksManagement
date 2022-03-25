12002809a6cb7d948083c9948581fec3d1beb753
pramodjadhav09 <impramodj@gmail.com>
2022-03-25 14:58:58 +0530

    userSchema Done

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,47 +1,38 @@
 const mongoose= require('mongoose')
 
 
-
-
 const userSchema= new mongoose.Schema({
 
     title: {
         type:String,
-        required:[true, "Title is required"],
+        required:true,
         enum:["Mr","Mrs","Miss"]
     },
     name:{
         type:String,
-        required:[true, "Name is required"]
+        required:true
     },
     phone:{
         type:String,
-        required:[true, "Phone is required"],
-        unique: [true, "User with this phone already exist"],
-        validate: {
-            validator: function validatePhoneNumber(number) {
-                var re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-
-                return re.test(number);
-            }, message: "Please provide a valid mobile number"
-        }
-    },
+        required:true,
+        unique: true,
+        },
     email:{
         type:String,
-        required:[true, "Email is required"],
-        unique: [true, "User with this email already exist"],
-        validate: {
-            validator: function (email) {
-                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
-            }, message: "Please provide a valid email address"
-        }
+        required:true,
+        unique: true,
     },
     password:{
         type:String,
-        required: [true, "Password is required"],
+        required: true,
+    },
+    address: {
+        street:{type:String},
+        city: {type:String},
+        pincode: {type:String}
     }
 
 
+}, {timestamps:true})
 
-
-})
+module.exports= mongoose.model ('User', userSchema)

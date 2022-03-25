77084ca0a31fc88ea18b90958622faddfd16b24c
pramodjadhav09 <impramodj@gmail.com>
2022-03-25 16:30:06 +0530

    createUser API

---
+++ src/controllers/userController.js
@@ -0,0 +1,15 @@
+const userModel = require("../models/userModel")
+
+
+
+const createUser = async function (req, res) {
+    const data = req.body;
+    userCreated = await userModel.create(data);
+
+    res.status(201).send({status: true, message:"User created successfully", data:data})
+}
+
+
+
+
+module.exports.createUser=createUser
